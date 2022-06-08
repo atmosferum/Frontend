ca0d8d9959d1edd0bbd4d8b147c66bd78c4e3e8a
sadnesszephyr <tima@nopublic.ru>
2022-06-09 00:43:25 +0300

    Initial Commit

---
+++ pages/_document.tsx
@@ -0,0 +1,15 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+
+export default function Document() {
+	return (
+		<Html>
+			<Head>
+				<link rel="icon" href="/favicon.png" />
+			</Head>
+			<body>
+				<Main />
+				<NextScript />
+			</body>
+		</Html>
+	)
+}
---
+++ pages/index.tsx
@@ -0,0 +1,16 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Time Manager</title>
+      </Head>
+      <h1>Hello World</h1>
+    </>
+  )
+}
+
+export default Home
