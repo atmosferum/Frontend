99a0f65ed69bbf1bd6a1e6de06aaa5c49fe31542
Makar Kryazhev <6o3kmm@mail.ru>
2022-06-21 08:35:21 +0000

    Resolve "Настроить линтеры, prettier, прекомит-хуки"

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,15 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import Head from "next/head";
 
 const Home: NextPage = () => {
-  return (
-    <>
-      <Head>
-        <title>Time Manager</title>
-      </Head>
-      <h1>Hello World</h1>
-    </>
-  )
-}
+    return (
+        <>
+            <Head>
+                <title>Time Manager</title>
+            </Head>
+            <h1>Hello World</h1>
+        </>
+    );
+};
 
-export default Home
+export default Home;
