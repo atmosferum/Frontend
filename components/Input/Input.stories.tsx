f55139e7fb3f7a39f5b15214f462359b577be7dd
Makar Kryazhev <6o3kmm@mail.ru>
2022-06-21 13:49:19 +0000

    Resolve "Добавить компонент «Поле для ввода»"

---
+++ components/Input/Input.stories.tsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { Input } from "./Input";
+
+export default {
+  title: "Input",
+  component: Input,
+  argTypes: {
+    error: {
+      name: "error",
+      type: "boolean",
+      defaultValue: false,
+    },
+    placeholder: {
+      type: "string",
+      defaultValue: "Плейсхолдер",
+    },
+    value: {
+      type: "string",
+    },
+  },
+} as ComponentMeta<typeof Input>;
+
+export const Primary: ComponentStory<typeof Input> = (args) => {
+  return (
+    <Input
+      error={args.error}
+      value={args.value}
+      placeholder={args.placeholder}
+    />
+  );
+};
---
+++ components/Input/Input.tsx
@@ -0,0 +1,16 @@
+import styles from "./style.module.scss";
+import { InputHTMLAttributes } from "react";
+import classNames from "classnames/bind";
+
+const cx = classNames.bind(styles);
+
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  error?: boolean;
+}
+
+export function Input(props: InputProps) {
+  const { error = false, ...restProps } = props;
+  return (
+    <input type="text" className={cx("input", { error })} {...restProps} />
+  );
+}
