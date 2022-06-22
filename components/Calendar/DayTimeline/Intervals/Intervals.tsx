87b652cff7ce6662bd1c03fe0159297ed35185de
fedor-resh <87930362+fedor-resh@users.noreply.github.com>
2022-06-22 16:59:59 +0300

    fix clockFace of intervals

--- components/Calendar/DayTimeline/Intervals/Intervals.tsx
+++ components/Calendar/DayTimeline/Intervals/Intervals.tsx
@@ -1,10 +1,10 @@
-import React from "react";
-import { DraggingElement, Interval } from "../../../../types";
-import { getClockFace, isEqualDays } from "../../utils";
-import { HEIGHT_OF_CELL } from "../DayTimeline";
-import s from "./Intervals.module.scss";
-import classNames from "classnames/bind";
-import { months } from "../../consts";
+import React from 'react';
+import { DraggingElement, Interval } from '../../../../types';
+import { getClockFace, isEqualDays } from '../../utils';
+import { HEIGHT_OF_CELL } from '../DayTimeline';
+import s from './Intervals.module.scss';
+import classNames from 'classnames/bind';
+import { months } from '../../consts';
 
 const cx = classNames.bind(s);
 
@@ -28,10 +28,16 @@ const Intervals = (props: Props) => {
         const hoursOfEnd = end.getHours() + end.getMinutes() / 60;
         const drawFrom = isStartToday ? hoursOfStart : 0;
         const drawTo = isEndToday ? hoursOfEnd : 24;
-        const clockFace = isEqualDays(start, end)
-          ? `${getClockFace(hoursOfStart)} — ${getClockFace(hoursOfEnd)}`
-          : `${months[start.getMonth()]} ${start.getDate()} ${getClockFace(hoursOfStart)}
-           — ${months[end.getMonth()]} ${end.getDate()} ${getClockFace(hoursOfEnd)}`;
+        const clockFace = isEqualDays(start, end) ? (
+          <p>{`${getClockFace(hoursOfStart)} — ${getClockFace(hoursOfEnd)}`}</p>
+        ) : (
+          <>
+            <p>{`${months[start.getMonth()]} ${start.getDate()} ${getClockFace(
+              hoursOfStart,
+            )} — `}</p>
+            <p>{`${months[end.getMonth()]} ${end.getDate()} ${getClockFace(hoursOfEnd)}`}</p>
+          </>
+        );
         const style = {
           top: drawFrom * HEIGHT_OF_CELL * 2,
           height: (drawTo - drawFrom) * 2 * HEIGHT_OF_CELL - margin * 2,
@@ -43,22 +49,22 @@ const Intervals = (props: Props) => {
           margin,
         };
         return (
-          <div key={id} style={style} className={cx("interval")}>
-            <p className={cx("clockFace")}>{clockFace}</p>
+          <div key={id} style={style} className={cx('interval')}>
+            <div className={cx('clockFace')}>{clockFace}</div>
             {draggable && (
               <>
                 <div
-                  className={cx("top")}
+                  className={cx('top')}
                   onMouseDown={() => {
-                    document.body.style.cursor = "row-resize";
-                    draggingElement.current = { id, part: "start" };
+                    document.body.style.cursor = 'row-resize';
+                    draggingElement.current = { id, part: 'start' };
                   }}
                 />
                 <div
-                  className={cx("bottom")}
+                  className={cx('bottom')}
                   onMouseDown={() => {
-                    document.body.style.cursor = "row-resize";
-                    draggingElement.current = { id, part: "end" };
+                    document.body.style.cursor = 'row-resize';
+                    draggingElement.current = { id, part: 'end' };
                   }}
                 />
               </>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,75 @@
-import type { NextPage } from "next";
-import Head from "next/head";
+import type { NextPage } from 'next';
+import Head from 'next/head';
+import { Calendar } from '../components/Calendar';
+import { useRef, useState } from 'react';
+import { getDateOfMonday } from '../utils';
+import s from '../styles/index.module.scss';
+import { Button } from '../components/Button';
+import { MS_IN_DAY } from '../consts';
+import { Input } from '../components/Input';
 
 const Home: NextPage = () => {
+  const [adminIntervals, setAdminIntervals] = useState([]);
+  const [myIntervals, setMyIntervals] = useState([]);
+  const [dateOfMonday, setDateOfMonday] = useState(getDateOfMonday(new Date()));
+  const [isResults, setIsResults] = useState(false);
+  const isAdmin = false;
+  const title = 'Название события';
+  const draggingElement = useRef(null);
+
+  function previousWeek() {
+    setDateOfMonday(new Date(dateOfMonday.getTime() - MS_IN_DAY * 7));
+  }
+
+  function nextWeek() {
+    setDateOfMonday(new Date(dateOfMonday.getTime() + MS_IN_DAY * 7));
+  }
+
+  function createEvent() {}
+  function saveIntervals() {}
+  function goToResults() {}
+  function goToVoting() {}
+
+  const Buttons = () => {
+    if (isAdmin) {
+      return <Button onClick={createEvent}>Создать событие</Button>;
+    } else {
+      if (isResults) {
+        return <Button onClick={goToVoting}>к голосованию</Button>;
+      } else {
+        return (
+          <>
+            <Button onClick={goToResults}>результаты</Button>
+            <Button onClick={saveIntervals}>сохранить</Button>
+          </>
+        );
+      }
+    }
+  };
+
+  const propsForCalendar = {
+    adminIntervals,
+    myIntervals,
+    draggingElement,
+    dateOfMonday,
+    setIntervals: isAdmin ? setAdminIntervals : setMyIntervals,
+    isAdmin,
+    isResults,
+  };
   return (
-    <>
+    <div className={s.window}>
       <Head>
         <title>Time Manager</title>
       </Head>
-    </>
+      <div className={s.header}>
+        <p>week slider</p>
+        {!isAdmin ? <h1>{title}</h1> : <Input placeholder="введите название события" />}
+        <div>
+          <Buttons />
+        </div>
+      </div>
+      <Calendar {...propsForCalendar} />
+    </div>
   );
 };
 
