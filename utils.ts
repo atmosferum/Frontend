962a46a2f75fdbeb50a63416d7b3b41e3abaf168
6ocra3 <58596070+6ocra3@users.noreply.github.com>
2022-06-23 11:40:04 +0300

    add axios

--- utils.ts
+++ utils.ts
@@ -3,8 +3,10 @@ import { MS_IN_DAY } from './consts';
 export function getDateOfMonday(date: Date): Date {
   return new Date(date.getTime() - MS_IN_DAY * (date.getDay() ? date.getDay() - 1 : 6));
 }
-
-export async function loginPost() {}
+const url = 'whattime.ru/api/v1';
+export async function loginPost() {
+  const req = `${url}/login`;
+}
 
 export async function currentUserGet() {}
 
