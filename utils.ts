06401b9520a3050e3871b282fefc79a5ac509965
6ocra3 <58596070+6ocra3@users.noreply.github.com>
2022-06-23 11:02:19 +0300

    add templates for requests

--- utils.ts
+++ utils.ts
@@ -3,3 +3,19 @@ import { MS_IN_DAY } from './consts';
 export function getDateOfMonday(date: Date): Date {
   return new Date(date.getTime() - MS_IN_DAY * (date.getDay() ? date.getDay() - 1 : 6));
 }
+
+export async function loginPost() {}
+
+export async function currentUserGet() {}
+
+export async function logoutGet() {}
+
+export async function eventsPost() {}
+
+export async function eventsIdGet(id: number) {}
+
+export async function eventsIntervalsPost(id: number) {}
+
+export async function eventsIntervalsGet(id: number) {}
+
+export async function eventsResultGet(id: number) {}
