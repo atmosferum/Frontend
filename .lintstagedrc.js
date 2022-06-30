a04c2abd167268ff3800c8a4c8daff117e9c0875
6ocra3 <6o3kmm@mail.ru>
2022-06-30 16:04:02 +0300

    add prettier v2

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -1,11 +1,14 @@
-const path = require("path");
+const path = require('path');
 
 const buildEslintCommand = (filenames) =>
-  `yarn eslint ${filenames
-    .map((f) => path.relative(process.cwd(), f))
-    .join(" --fix ")} `;
+  `yarn eslint ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --fix ')} `;
+
+const buildPrettierCommand = (filenames) =>{
+  `yarn prettier --write ${filenames.map((f) => path.relative(process.cwd(), f)).join(' ')} `
+}
+
 
 module.exports = {
-  "*.{js,jsx,ts,tsx}": [buildEslintCommand, "yarn typecheck"],
-  "*.scss": ["prettier --write"],
+  '*.{js,jsx,ts,tsx}': [buildEslintCommand, buildPrettierCommand, 'yarn typecheck'],
+  '*.scss': ['prettier --write'],
 };
--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,11 +1,11 @@
-import '!style-loader!css-loader!sass-loader!../src/styles/globals.scss'
+import '!style-loader!css-loader!sass-loader!../src/styles/globals.scss';
 
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
