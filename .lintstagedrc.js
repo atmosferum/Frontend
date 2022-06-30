4c4d1642ddca47746d6ff41bc1598284830b05af
6ocra3 <6o3kmm@mail.ru>
2022-06-30 16:04:57 +0300

    add lintstaged

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -3,10 +3,8 @@ const path = require('path');
 const buildEslintCommand = (filenames) =>
   `yarn eslint ${filenames.map((f) => path.relative(process.cwd(), f)).join(' --fix ')} `;
 
-const buildPrettierCommand = (filenames) =>{
-  `yarn prettier --write ${filenames.map((f) => path.relative(process.cwd(), f)).join(' ')} `
-}
-
+const buildPrettierCommand = (filenames) =>
+  `yarn prettier --write ${filenames.map((f) => path.relative(process.cwd(), f)).join(' ')} `;
 
 module.exports = {
   '*.{js,jsx,ts,tsx}': [buildEslintCommand, buildPrettierCommand, 'yarn typecheck'],
