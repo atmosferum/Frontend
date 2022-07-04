b292420a9f7705612138c9601c750887b014bce7
6ocra3 <6o3kmm@mail.ru>
2022-07-04 21:42:38 +0300

    add styled popover

--- src/components/Popover/Popover.tsx
+++ src/components/Popover/Popover.tsx
@@ -1,18 +1,8 @@
 import React, { ReactNode, useState } from 'react';
-import {
-  Root,
-  Trigger,
-  Content,
-  Close,
-  Arrow,
-  Anchor,
-  PopoverProps as RadixPopoverProps,
-  PopoverAnchor,
-} from '@radix-ui/react-popover';
-import { X } from 'react-feather';
-import { Button } from '../Button';
 import styles from './style.module.scss';
 import cn from 'classnames/bind';
+import { X } from 'react-feather';
+
 const cx = cn.bind(styles);
 
 interface PopoverProps {
@@ -24,17 +14,19 @@ interface PopoverProps {
 }
 
 export function Popover(props: PopoverProps) {
-  const { children, x, y } = props;
+  const { children, x, y, close } = props;
   const [open, setOpen] = useState(props.open);
   const styles = {
     display: open ?? false ? 'block' : 'none',
-    top: '0',
-    left: '0',
+    top: '200px',
+    left: '200px',
     width: '200px',
   };
   return (
-    <div style={{ position: 'absolute', ...styles }}>
-      <button onClick={() => setOpen(false)}></button>
+    <div style={{ position: 'absolute', ...styles }} className={cx('content')}>
+      <div className={cx('close')}>
+        <X onClick={close ?? (() => setOpen(false))} />
+      </div>
       <div>
         Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur sunt sint iste modi
         aliquam fugiat nihil excepturi natus cumque hic corrupti esse officia blanditiis impedit, in
