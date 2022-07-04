a85c0da5f6bff8ee8d17dcf5876a3d1b52ddefae
6ocra3 <6o3kmm@mail.ru>
2022-07-04 21:20:11 +0300

    add non-styled popover

--- src/components/Popover/Popover.stories.tsx
+++ src/components/Popover/Popover.stories.tsx
@@ -1,21 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { ComponentStory, ComponentMeta } from '@storybook/react';
 import { Popover } from './Popover';
-import { Button } from '../Button';
-import { Input } from '../Input';
 
 export default {
-  title: 'Popover',
+  title: 'RadixPopover',
   component: Popover,
 } as ComponentMeta<typeof Popover>;
 
 export const PopoverStory: ComponentStory<typeof Popover> = (args) => {
+  const [popoverX, setPopoverX] = useState();
+  const [popoverY, setPopoverY] = useState();
+  const [popoverOpen, setPopoverOpen] = useState(true);
+
+  function openPopover(e: any) {
+    setPopoverX(e.clientX);
+    setPopoverY(e.clientY);
+    setPopoverOpen(true);
+    console.log(e.clientX + ' ' + e.clientY);
+  }
+
   return (
     <>
-      <Popover trigger={<Button>Открыть Popover</Button>}>
-        {/* <Button>Сохранить</Button>
-            <Input></Input> */}
-      </Popover>
+      <div style={{ background: 'coral', width: '200px', height: '500px' }}></div>
+      <Popover x={popoverX} y={popoverY} open={popoverOpen}></Popover>
     </>
   );
 };
--- src/components/Popover/Popover.tsx
+++ src/components/Popover/Popover.tsx
@@ -1,30 +1,45 @@
-import React, { ReactNode } from 'react';
+import React, { ReactNode, useState } from 'react';
 import {
   Root,
   Trigger,
   Content,
   Close,
   Arrow,
+  Anchor,
   PopoverProps as RadixPopoverProps,
+  PopoverAnchor,
 } from '@radix-ui/react-popover';
+import { X } from 'react-feather';
 import { Button } from '../Button';
+import styles from './style.module.scss';
+import cn from 'classnames/bind';
+const cx = cn.bind(styles);
 
-interface PopoverProps extends RadixPopoverProps {
-  children: ReactNode;
-  trigger: ReactNode;
+interface PopoverProps {
+  children?: ReactNode;
+  open?: boolean;
+  x?: number;
+  y?: boolean;
+  close?: () => void;
 }
 
 export function Popover(props: PopoverProps) {
-  const { children, trigger, ...restProps } = props;
-
+  const { children, x, y } = props;
+  const [open, setOpen] = useState(props.open);
+  const styles = {
+    display: open ?? false ? 'block' : 'none',
+    top: '0',
+    left: '0',
+    width: '200px',
+  };
   return (
-    <Root>
-      <Trigger asChild>{trigger}</Trigger>
-      <Content>
-        {children}
-        <Close />
-        <Arrow />
-      </Content>
-    </Root>
+    <div style={{ position: 'absolute', ...styles }}>
+      <button onClick={() => setOpen(false)}></button>
+      <div>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Consectetur sunt sint iste modi
+        aliquam fugiat nihil excepturi natus cumque hic corrupti esse officia blanditiis impedit, in
+        voluptatibus odio itaque non?
+      </div>
+    </div>
   );
 }
